7483fe5f2174d5f1b0250fb2a8db7215fb047ba4
JustToBeBetter <li@li.local>
2016-04-29 13:54:39 +0800

    蓝牙模块
    蓝牙通信demo

---
+++ BLECentralPeripheralTransferDemo/LJZBTEPeripheralViewController.h
@@ -0,0 +1,13 @@
+//
+//  LJZBTEPeripheralViewController.h
+//  BLECentralPeripheralTransferDemo
+//
+//  Created by 李金柱 on 16/4/28.
+//  Copyright © 2016年 likeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJZBTEPeripheralViewController : UIViewController
+
+@end
